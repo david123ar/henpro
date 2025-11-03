de1aebf9fa0336b453d09f388ea76888bc8c8eb9
rishabh-star <risingmerta@gmail.com>
2025-11-03 23:07:02 +0530

    first commit

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from "next/link";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../footer/Footer";
@@ -5,20 +6,22 @@ import Footer from "../footer/Footer";
 export default function NotFound() {
     return (
         <>
-            <Navbar now={false} />
-            <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
-                <h1 className="text-4xl font-bold mb-4">404 - User Not Found 😔</h1>
-                <p className="text-gray-400 mb-6 text-center">
-                    Sorry, we couldn’t find the page or user you’re looking for.
-                </p>
-                <Link
-                    href="/"
-                    className="px-6 py-3 bg-[#FF9741] hover:bg-[#ffac67] text-white rounded-2xl transition-all"
-                >
-                    Return to Home
-                </Link>
-            </div>
-            <Footer />
+            <SessionProvider>
+                <Navbar now={false} />
+                <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+                    <h1 className="text-4xl font-bold mb-4">404 - User Not Found 😔</h1>
+                    <p className="text-gray-400 mb-6 text-center">
+                        Sorry, we couldn’t find the page or user you’re looking for.
+                    </p>
+                    <Link
+                        href="/"
+                        className="px-6 py-3 bg-[#FF9741] hover:bg-[#ffac67] text-white rounded-2xl transition-all"
+                    >
+                        Return to Home
+                    </Link>
+                </div>
+                <Footer />
+            </SessionProvider>
         </>
     );
 }
